1d9d1a4e64826172deee475e8d7427d54d400792
DJX <apple@appledeMacBook-Pro.local>
2019-08-25 13:37:11 +0800

    add

--- src/search/BinSearch.java
+++ src/search/BinSearch.java
@@ -1,4 +1,43 @@
 package search;
 
+import java.util.Arrays;
+
 public class BinSearch {
+
+    public static int getIndex_while(int[] arr, int key) {
+        int low = 0;
+        int high = arr.length - 1;
+
+        if (low > high || key < arr[low] || key > arr[high]) {
+            return -1;
+        }
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (key == arr[mid]) {
+                return mid;
+            } else if (key > arr[mid]) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return -1;
+    }
+
+    public static int getIndex_digui(int[] arr, int key, int low, int high) {
+
+        if (low > high || key < arr[low] || key > arr[high]) {
+            return -1;
+        }
+        int mid = (low + high) / 2;
+        if (key > arr[mid]) {
+            return getIndex_digui(arr, key, mid + 1, high);
+        } else if (key < arr[mid]) {
+            return getIndex_digui(arr, key, low, mid - 1);
+        } else {
+            return mid;
+        }
+    }
+
 }
