43282aa32031f9ee3aa692a89d2e1b5d17ab4e65
dongjingxiong <dopngjingxiong@beisen.com>
2019-08-26 17:48:59 +0800

    add

--- src/common_interview/SingleList.java
+++ src/common_interview/SingleList.java
@@ -4,12 +4,17 @@ public class SingleList {
     int value;
     SingleList next;
 
-
     public SingleList(int value) {
         this.value = value;
         this.next = null;
     }
 
+    @Override
+    public String toString(){
+        return this.value + "-->" + (next != null ? next.toString() : "null");
+    }
+
+    //判断单链表是否有环
     public static boolean isLoop(SingleList head){
         if (head == null) {
             return false;
@@ -28,4 +33,31 @@ public class SingleList {
         }
         return false;
     }
+
+    //单链表的逆置
+    //递归用栈的思想，先把头节点进栈，接着头节点的下一节点再进栈，直到尾节点的位置
+    public static SingleList reverse(SingleList node){
+        System.out.println("before:" + node);
+        if(node == null || node.next == null){
+            System.out.println("after:" + node) ;
+            return node;
+        }
+        SingleList head = reverse(node.next);
+        node.next.next = node;
+        node.next = null;
+        System.out.println("after:"+ head);
+        return head;
+    }
+
+    public static void main(String[] args) {
+        SingleList a = new SingleList(1);
+        SingleList b = new SingleList(2);
+        SingleList c = new SingleList(3);
+        SingleList d = new SingleList(4);
+        a.next = b;
+        b.next = c;
+        c.next = d;
+        reverse(a);
+    }
+
 }
