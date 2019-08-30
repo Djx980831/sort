e69234efd212fb4acd8058aafbce8e44c552a4f4
dongjingxiong <dongjingxiong@beisen.com>
2019-08-30 17:16:53 +0800

    feibo

---
+++ src/common_interview/FeiBo.java
@@ -0,0 +1,21 @@
+package common_interview;
+
+public class FeiBo {
+
+    public static int getSum(int n){
+        int sum = 0;
+        int a = 1;
+        int b = 1;
+        sum = a + b;
+        for(int i = 3; i < n; i++){
+            a = b;
+            b = sum;
+            sum = a + b;
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getSum(10));
+    }
+}
