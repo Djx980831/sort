a9bd4e742a1a43abe3796662c1f90429dc696412
dongjingxiong <dongjingxiong@beisen.com>
2019-09-02 17:01:20 +0800

    getDaoShuKey

--- src/common_interview/AA.java
+++ src/common_interview/AA.java
@@ -1,8 +1,31 @@
 package common_interview;
 
+import java.util.Scanner;
+
 public class AA {
+
     public static void main(String[] args) {
-        System.out.println("test!");
-        System.out.println("I an AA");
+        Scanner scanner = new Scanner(System.in);
+        String str1 = "#" + scanner.next();
+        String str2 = "#" + scanner.next();
+        int[][] dp = new int[str1.length()][str2.length()];
+
+        // 初始化
+        for (int i = 1; i < str1.length(); i++)
+            dp[i][0] = i;
+        for (int i = 1; i < str2.length(); i++)
+            dp[0][i] = i;
+
+        for (int i = 1; i < str1.length(); i++) {
+            for (int j = 1; j < str2.length(); j++) {
+                if (str1.charAt(i) == str2.charAt(j))
+                    dp[i][j] = dp[i - 1][j - 1];
+                else {
+                    dp[i][j] = Math.min(dp[i - 1][j - 1],
+                            Math.min(dp[i][j - 1], dp[i - 1][j])) + 1;
+                }
+            }
+        }
+        System.out.println(dp[str1.length() - 1][str2.length() - 1]);
     }
 }
--- src/common_interview/SingleList.java
+++ src/common_interview/SingleList.java
@@ -60,21 +60,65 @@ public class SingleList {
         return list;
     }
 
+    public static SingleList getDaoShuKey(SingleList head, int k){
+        if(k <= 0 || head == null){
+            return null;
+        }
 
+        SingleList h1 = head;
+        SingleList h2 = head;
+
+        for(int i = 0; i < k; i++){
+            if(h2.next != null){
+                h2 = h2.next;
+            } else {
+                return null;
+            }
+        }
+
+        while (h2.next != null) {
+            h1 = h1.next;
+            h2 = h2.next;
+        }
+
+        return h1;
+    }
 
     public static void main(String[] args) {
+
         SingleList a = new SingleList(1);
         SingleList b = new SingleList(2);
         SingleList c = new SingleList(3);
         SingleList d = new SingleList(4);
+        SingleList e = new SingleList(5);
+        SingleList f = new SingleList(6);
+        SingleList g = new SingleList(7);
+        SingleList h = new SingleList(8);
+        SingleList i = new SingleList(9);
+        SingleList j = new SingleList(10);
+        SingleList k = new SingleList(11);
+        SingleList l = new SingleList(12);
+        SingleList m = new SingleList(13);
+
         a.next = b;
         b.next = c;
         c.next = d;
+        d.next = e;
+        e.next = f;
+        f.next = g;
+        g.next = h;
+        h.next = i;
+        i.next = j;
+        j.next = k;
+        k.next = l;
+        l.next = m;
         //reverse(a);
-        ArrayList<Integer> list = printList(a);
-        for(Integer integer : list){
-            System.out.println(integer);
-        }
+        SingleList result = getDaoShuKey(a, 6);
+        System.out.println(result);
+//        ArrayList<Integer> list = printList(a);
+//        for(Integer integer : list){
+//            System.out.println(integer);
+//        }
     }
 
     //非递归的方法其实就是顺着头结点往尾结点遍历，遍历期间把每个结点的nextNode替换掉，
