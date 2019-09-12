5b6e859cde521579d96d10cc12b44968d23a89b8
1530916212@qq.com <1530916212@qq.com>
2019-09-12 14:58:16 +0800

    test try-finally-return

---
+++ src/common_interview/TestTryFinally.java
@@ -0,0 +1,43 @@
+package common_interview;
+
+import java.util.ArrayList;
+
+public class TestTryFinally {
+
+    public static int aa() {
+        int x = 1;
+        try {
+            return ++x;
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            ++x;
+        }
+        return x;
+    }
+
+    public static ArrayList<Integer> test() {
+        ArrayList<Integer> list = new ArrayList<>();
+        try {
+            list.add(1);
+            list.add(2);
+            list.add(3);
+            list.add(4);
+            list.add(5);
+
+            return list;
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            list.add(6);
+        }
+        return list;
+    }
+
+    public static void main(String[] args) {
+       ArrayList<Integer> list = test();
+       for(Integer i : list){
+           System.out.println(i);
+       }
+    }
+}
